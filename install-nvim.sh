9ebf6fc150df92371d89324e97c27915df4dbc7b
RunAtTekky <varunrawat343@gmail.com>
2026-01-17 00:39:10 +0530

    if already exists, remove NVIM config

--- install-nvim.sh
+++ install-nvim.sh
@@ -3,6 +3,11 @@
 GHUB_REPO="https://github.com/RunAtTekky/kickstart-modular.nvim.git"
 NVIM_CFG="$HOME/.config/nvim"
 
+if [ -d "$NVIM_CFG" ]; then
+    echo "NVIM Config already present. Removing"
+    rm -rf "$NVIM_CFG"
+fi
+
 git clone "$GHUB_REPO" "$NVIM_CFG"
 cd "$NVIM_CFG"
 
