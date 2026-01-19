67ce5bcde52848df89bfc76828a2b0f37d16c5b8
RunAtTekky <varunrawat343@gmail.com>
2026-01-19 11:05:10 +0530

    Use current directory before sourcing each script
    add: Final message

--- install-all.sh
+++ install-all.sh
@@ -6,15 +6,22 @@ CURR_DIR="$(pwd)"
 source "./install-brew.sh"
 
 # Install all the packages in ./curr.conf
+cd "$CURR_DIR"
 source "./install-pkgs.sh"
 
+cd "$CURR_DIR"
 echo "Installing dotfiles"
 source "./install-dotfiles.sh"
 
 cd "$CURR_DIR"
 source "./install-oh-my-zsh.sh"
 
+cd "$CURR_DIR"
 echo "Installing NVIM with Runat's config"
 source "./install-nvim.sh"
 
+cd "$CURR_DIR"
 source "./install-aerospace.sh"
+
+echo "Almost everything RunAt uses is installed and configured."
+figlet -c 'DONE'
