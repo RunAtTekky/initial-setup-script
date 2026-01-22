348546b7eeb74938e04be2ab99088c62802d6aff
RunAtTekky <varunrawat343@gmail.com>
2026-01-22 23:49:42 +0530

    output brew list "$1" data to /dev/null

--- helper.sh
+++ helper.sh
@@ -5,14 +5,14 @@ export PERSONAL_REPO="$HOME/MySpace/personal"
 export DASH_LINE="--------------------------------"
 
 is_installed() {
-    brew list "$1"
+    brew list "$1" &>/dev/null
 }
 
 install_pkgs() {
   local packages=("$@")
   local to_install=()
 
-  echo "Trying to install ${packages[*]}"
+  echo "Asked to install: ${packages[*]}"
   for pkg in "${packages[@]}"; do
     if ! is_installed "$pkg" ; then
       to_install+=("$pkg")
@@ -20,7 +20,7 @@ install_pkgs() {
   done
 
   if [ ${#to_install[@]} -ne 0 ]; then
-    echo "Installing ${packages[*]}"
+    echo "Installing: ${packages[*]}"
     brew install "${to_install[@]}"
   fi
 }
